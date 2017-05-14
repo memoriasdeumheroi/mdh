345f4554123e5b0b8b3c43f8868d031c27d71c55
felipefrancischetto <felipe-matteus@hotmail.com>
2017-05-14 17:05:02 -0300

    implementação do cadastro dos dados do jogador, validações e tratamento de erros

--- src/main/java/br/com/sixtechsolutions/controller/CadastrarController.java
+++ src/main/java/br/com/sixtechsolutions/controller/CadastrarController.java
@@ -7,6 +7,7 @@ import br.com.sixtechsolutions.model.dao.JogadorDAO;
 import br.com.sixtechsolutions.model.database.Database;
 import br.com.sixtechsolutions.model.database.DatabaseFactory;
 import br.com.sixtechsolutions.model.dominio.Jogador;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -14,10 +15,12 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.PieChart;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -26,16 +29,19 @@ import javafx.scene.layout.VBox;
 
 public class CadastrarController implements Initializable, CenaControlada {
 
-    private ControladorDeCenas meuControlador;
+    ControladorDeCenas meuControlador;
     private final Database database = DatabaseFactory.getDatabase("mysql");
     private final Connection connection = database.conectar();
     private final JogadorDAO jogadorDAO = new JogadorDAO();
     private Jogador referencia;
     private boolean confirmaClicksexo = false;
     private boolean confirmaClickpersonagem = false;
+    private String menssagemErro;
+    private String sexo;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        visualizarDadosPersonagem(false);
         jogadorDAO.setConnection(connection);
     }
 
@@ -54,43 +60,43 @@ public class CadastrarController implements Initializable, CenaControlada {
     private HBox hbox;
 
     @FXML
-    private Button btnPersonagem1;
+    private Button btnBomberman;
 
     @FXML
-    private Button btnPersonagem2;
+    private Button btnCrash;
 
     @FXML
-    private Button btnPersonagem3;
+    private Button btnDonkeyKong;
 
     @FXML
-    private Button btnPersonagem4;
+    private Button btnDukeNukem;
 
     @FXML
-    private Button btnPersonagem7;
+    private Button btnKratos;
 
     @FXML
-    private Button btnPersonagem5;
+    private Button btnLaraCroft;
 
     @FXML
-    private Button btnPersonagem8;
+    private Button btnMegaman;
 
     @FXML
-    private Button btnPersonagem6;
+    private Button btnMario;
 
     @FXML
-    private Button btnPersonagem9;
+    private Button btnPacman;
 
     @FXML
-    private Button btnPersonagem10;
+    private Button btnRyu;
 
     @FXML
-    private Button btnPersonagem11;
+    private Button btnSonic;
 
     @FXML
-    private Button btnPersonagem12;
+    private Button btnZelda;
 
     @FXML
-    private TextField textNickname;
+    private TextField txtLogin;
 
     @FXML
     private PasswordField passwordSenha;
@@ -111,10 +117,22 @@ public class CadastrarController implements Initializable, CenaControlada {
     private Label lblAvatar;
 
     @FXML
-    private Label lblConfirmaNickname;
+    private Button btnVoltar;
 
     @FXML
-    private Label lblConfirmaSenha;
+    private Label lblConfirmaNome;
+
+    @FXML
+    private Label lblConfirmaLogin;
+
+    @FXML
+    private Label lblConfirmaSenha1;
+
+    @FXML
+    private Label lblConfirmaSenha2;
+
+    @FXML
+    private TextField txtNome;
 
     @FXML
     private Label lblConfirmaSexo;
@@ -123,46 +141,80 @@ public class CadastrarController implements Initializable, CenaControlada {
     private Label lblConfirmaPersonagem;
 
     @FXML
-    private PieChart grafico;
+    private Label lblPersonagem;
 
     @FXML
-    private Button voltar;
+    private Label lblNomePersonagem;
+
+    @FXML
+    private Label lblAtaquePersonagem;
+
+    @FXML
+    private Label lblDefesaPersonagem;
+
+    @FXML
+    private Label lblAgilidadePersonagem;
+
+    @FXML
+    private ProgressBar progressBarAtaque;
+
+    @FXML
+    private ProgressBar progressBarDefesa;
+
+    @FXML
+    private ProgressBar progressBarAgilidade;
 
     public boolean validarDados() {
-        String menssagemErro = "";
-        if (textNickname.getText() == null || textNickname.getText().length() == 0) {
+        menssagemErro = "";
+
+        if (txtNome.getText() == null || txtNome.getText().length() == 0) {
             menssagemErro += "Nome inválido!\n";
-            lblConfirmaNickname.setText("Coloque um nickname!");
+            lblConfirmaNome.getStyleClass().set(1, "lbl-errado");
         } else {
-            lblConfirmaNickname.setText("");
+            lblConfirmaNome.getStyleClass().set(1, "lbl-certo");
+        }
+
+        if (txtLogin.getText() == null || txtLogin.getText().length() == 0) {
+            menssagemErro += "Login inválido!\n";
+            lblConfirmaLogin.getStyleClass().set(1, "lbl-errado");
+        } else {
+            lblConfirmaLogin.getStyleClass().set(1, "lbl-certo");
         }
 
         if (passwordSenha.getText() == null || passwordSenha.getText().length() == 0) {
             menssagemErro += "Senha inválida!\n";
-            lblConfirmaSenha.setText("Coloque uma senha!");
+            lblConfirmaSenha1.getStyleClass().set(1, "lbl-errado");
         } else {
-            lblConfirmaSenha.setText("");
+            lblConfirmaSenha1.getStyleClass().set(1, "lbl-certo");
         }
 
         if (passwordConfirma.getText() == null || passwordConfirma.getText().length() == 0) {
-            menssagemErro += "Senha inválida!\n";
-            lblConfirmaSenha.setText("Coloque a confirmação da senhas!");
+            menssagemErro += "Confirmaçãp inválida!\n";
+            lblConfirmaSenha2.getStyleClass().set(1, "lbl-errado");
         } else {
-            lblConfirmaSenha.setText("");
+            lblConfirmaSenha2.getStyleClass().set(1, "lbl-certo");
+        }
+
+        if (passwordSenha.getText().equals(passwordConfirma.getText())) {
+            //
+        } else {
+            menssagemErro += "Senha diferente da confirmação!\n";
+            lblConfirmaSenha1.getStyleClass().set(1, "lbl-errado");
+            lblConfirmaSenha2.getStyleClass().set(1, "lbl-errado");
         }
 
         if (confirmaClicksexo == false) {
             menssagemErro += "Sexo não selecionado!\n";
-            lblConfirmaSexo.setText("Sexo não selecionado!\n");
+            lblConfirmaSexo.getStyleClass().set(1, "lbl-errado");
         } else {
-            lblConfirmaSexo.setText("");
+            lblConfirmaSexo.getStyleClass().set(1, "lbl-certo");
         }
 
         if (confirmaClickpersonagem == false) {
             menssagemErro += "Personagem não selecionado!\n";
-            lblConfirmaPersonagem.setText("Personagem não selecionado!\n");
+            lblConfirmaPersonagem.getStyleClass().set(1, "lbl-errado");
         } else {
-            lblConfirmaPersonagem.setText("");
+            lblConfirmaPersonagem.getStyleClass().set(1, "lbl-certo");
         }
 
         if (menssagemErro.length() == 0) {
@@ -174,93 +226,162 @@ public class CadastrarController implements Initializable, CenaControlada {
         }
     }
 
-    @FXML
-    public void actionBtnCadastrar(ActionEvent event) throws SQLException {
-        if (validarDados() == true) {
-            Jogador jogador = new Jogador("felipe", "f", "alessandra", "1236");
-            jogadorDAO.inserir(jogador);
-        } else {
-            System.out.println("não foi possivel");
-        }
+    private void visualizarDadosPersonagem(boolean estado) {
+        progressBarAtaque.setVisible(estado);
+        progressBarDefesa.setVisible(estado);
+        progressBarAgilidade.setVisible(estado);
+        lblAtaquePersonagem.setVisible(estado);
+        lblDefesaPersonagem.setVisible(estado);
+        lblAgilidadePersonagem.setVisible(estado);
     }
 
     @FXML
-    public void radioActionMasculino(ActionEvent event) {
+    public void actionBtnVoltar(ActionEvent event) {
+        lblConfirmaNome.getStyleClass().set(1, "lbl-nulo");
+        lblConfirmaLogin.getStyleClass().set(1, "lbl-nulo");
+        lblConfirmaSenha1.getStyleClass().set(1, "lbl-nulo");
+        lblConfirmaSenha2.getStyleClass().set(1, "lbl-nulo");
+        lblConfirmaSexo.getStyleClass().set(1, "lbl-nulo");
+        lblConfirmaPersonagem.getStyleClass().set(1, "lbl-nulo");
+        lblPersonagem.getStyleClass().set(1, "lbl-nulo");
+        lblNomePersonagem.setText("");
+        txtNome.setText("");
+        txtLogin.setText("");
+        passwordSenha.setText("");
+        passwordConfirma.setText("");
+        visualizarDadosPersonagem(false);
+        meuControlador.setScreen(Main.cenaPrincipal);
+    }
+
+    @FXML
+    private void actionBtnCadastrar(ActionEvent event) throws SQLException, IOException {
+        if (validarDados() != true) {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("");
+            alert.setHeaderText(null);
+            alert.setContentText(menssagemErro);
+            alert.showAndWait();
+        } else {
+            referencia = new Jogador();
+            referencia.setLogin(txtLogin.getText());
+            referencia.setSenha(passwordSenha.getText());
+            referencia.setNome(txtNome.getText());
+            referencia.setSexo(sexo);
+            jogadorDAO.inserir(referencia);
+        }
+
+    }
+
+    @FXML
+    private void radioActionMasculino(ActionEvent event) {
         radioButtonMasculino.setSelected(true);
         radioButtonFeminino.setSelected(false);
         confirmaClicksexo = true;
+        sexo = "M";
     }
 
     @FXML
-    public void radioActionFeminino(ActionEvent event) {
+    private void radioActionFeminino(ActionEvent event) {
         radioButtonFeminino.setSelected(true);
         radioButtonMasculino.setSelected(false);
         confirmaClicksexo = true;
+        sexo = "F";
     }
 
     @FXML
-    void actionBtnPersonagem1(ActionEvent event) {
+    private void actionBtnBomberman(ActionEvent event) {
         confirmaClickpersonagem = true;
+        visualizarDadosPersonagem(true);
+        lblPersonagem.getStyleClass().set(1, "lbl-cadastro-personagem-bomberman");
+        lblNomePersonagem.setText("BOMBERMAN");
     }
 
     @FXML
-    void actionBtnPersonagem2(ActionEvent event) {
+    private void actionBtnCrash(ActionEvent event) {
         confirmaClickpersonagem = true;
+        visualizarDadosPersonagem(true);
+        lblPersonagem.getStyleClass().set(1, "lbl-cadastro-personagem-crash");
+        lblNomePersonagem.setText("CRASH");
     }
 
     @FXML
-    void actionBtnPersonagem3(ActionEvent event) {
+    private void actionBtnDonkeyKong(ActionEvent event) {
         confirmaClickpersonagem = true;
+        visualizarDadosPersonagem(true);
+        lblPersonagem.getStyleClass().set(1, "lbl-cadastro-personagem-donkeykong");
+        lblNomePersonagem.setText("DONKEY KONG");
     }
 
     @FXML
-    void actionBtnPersonagem4(ActionEvent event) {
+    private void actionBtnDukeNukem(ActionEvent event) {
         confirmaClickpersonagem = true;
+        visualizarDadosPersonagem(true);
+        lblPersonagem.getStyleClass().set(1, "lbl-cadastro-personagem-dukenukem");
+        lblNomePersonagem.setText("DUKE NUKEM");
     }
 
     @FXML
-    void actionBtnPersonagem5(ActionEvent event) {
+    private void actionBtnKratos(ActionEvent event) {
         confirmaClickpersonagem = true;
+        visualizarDadosPersonagem(true);
+        lblPersonagem.getStyleClass().set(1, "lbl-cadastro-personagem-kratos");
+        lblNomePersonagem.setText("KRATOS");
     }
 
     @FXML
-    void actionBtnPersonagem6(ActionEvent event) {
+    private void actionBtnLaraCroft(ActionEvent event) {
         confirmaClickpersonagem = true;
+        visualizarDadosPersonagem(true);
+        lblPersonagem.getStyleClass().set(1, "lbl-cadastro-personagem-laracroft");
+        lblNomePersonagem.setText("LARA CROFT");
     }
 
     @FXML
-    void actionBtnPersonagem7(ActionEvent event) {
+    private void actionBtnMegaman(ActionEvent event) {
         confirmaClickpersonagem = true;
+        visualizarDadosPersonagem(true);
+        lblPersonagem.getStyleClass().set(1, "lbl-cadastro-personagem-megaman");
+        lblNomePersonagem.setText("MEGAMAN");
     }
 
     @FXML
-    void actionBtnPersonagem8(ActionEvent event) {
+    private void actionBtnMario(ActionEvent event) {
         confirmaClickpersonagem = true;
+        visualizarDadosPersonagem(true);
+        lblPersonagem.getStyleClass().set(1, "lbl-cadastro-personagem-mario");
+        lblNomePersonagem.setText("MARIO");
     }
 
     @FXML
-    void actionBtnPersonagem9(ActionEvent event) {
+    private void actionBtnPacman(ActionEvent event) {
         confirmaClickpersonagem = true;
+        visualizarDadosPersonagem(true);
+        lblPersonagem.getStyleClass().set(1, "lbl-cadastro-personagem-pacman");
+        lblNomePersonagem.setText("PAC-MAN");
     }
 
     @FXML
-    void actionBtnPersonagem10(ActionEvent event) {
+    private void actionBtnRyu(ActionEvent event) {
         confirmaClickpersonagem = true;
+        visualizarDadosPersonagem(true);
+        lblPersonagem.getStyleClass().set(1, "lbl-cadastro-personagem-ryu");
+        lblNomePersonagem.setText("RYU");
     }
 
     @FXML
-    void actionBtnPersonagem11(ActionEvent event) {
+    private void actionBtnSonic(ActionEvent event) {
         confirmaClickpersonagem = true;
+        visualizarDadosPersonagem(true);
+        lblPersonagem.getStyleClass().set(1, "lbl-cadastro-personagem-sonic");
+        lblNomePersonagem.setText("SONIC");
     }
 
     @FXML
-    void actionBtnPersonagem12(ActionEvent event) {
+    private void actionBtnZelda(ActionEvent event) {
         confirmaClickpersonagem = true;
-    }
-
-    @FXML
-    public void btnActionVoltar(ActionEvent event) {
-        meuControlador.setScreen(Main.cenaPrincipal);
+        visualizarDadosPersonagem(true);
+        lblPersonagem.getStyleClass().set(1, "lbl-cadastro-personagem-zelda");
+        lblNomePersonagem.setText("ZELDA");
     }
 
 }
